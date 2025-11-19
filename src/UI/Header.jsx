fc757cf405bc215b5d1936ba4978dc7d9a6c06da
Vasilis-Chatzidakis <vasilis.chatzidakis.00@gmail.com>
2025-11-19 18:29:55 +0200

    Rename company title

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-300 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        Fast React Pizza Co.
+        Pizza of Pleasure Co.
       </Link>
       <SearchOrder />
       <Username />
